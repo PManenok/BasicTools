045bb33e9eda44d806b8cff6adcdfd01a3facee4
ABivoina <hannabivoina@gmail.com>
2022-09-05 17:20:03 +0300

    Add case for SwitchManager from util module

--- app/src/main/java/by/esas/tools/screens/util/switchManager/UtilSwitchManagerFragment.kt
+++ app/src/main/java/by/esas/tools/screens/util/switchManager/UtilSwitchManagerFragment.kt
@@ -1,9 +1,15 @@
 package by.esas.tools.screens.util.switchManager
 
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.lifecycle.ViewModelProvider
 import by.esas.tools.R
 import by.esas.tools.base.AppFragment
+import by.esas.tools.customswitch.ISwitchHandler
 import by.esas.tools.databinding.FMainUtilSwitchManagerBinding
+import by.esas.tools.util.SwitchManager
+import com.google.android.material.switchmaterial.SwitchMaterial
 
 class UtilSwitchManagerFragment: AppFragment<UtilSwitchManagerVM, FMainUtilSwitchManagerBinding>() {
     override val fragmentDestinationId = R.id.utilSwitchManagerFragment
@@ -14,4 +20,76 @@ class UtilSwitchManagerFragment: AppFragment<UtilSwitchManagerVM, FMainUtilSwitc
         return ViewModelProvider(this, viewModelFactory.provideFactory()).get(UtilSwitchManagerVM::class.java)
     }
 
+    override fun provideSwitchableViews(): List<View?> {
+        return listOf(
+            binding.fMainUtilManagerBaseButton,
+            binding.fMainUtilManagerBaseEdit,
+            binding.fMainUtilManagerBaseTestSwitcher,
+            binding.fMainUtilManagerBaseSwitchMaterial
+        )
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.fMainUtilManagerBaseSwitcher.setSwitchHandler(object : ISwitchHandler {
+            override fun onSwitchChange(isChecked: Boolean) {
+                if (isChecked)
+                    enableControls()
+                else
+                    disableControls()
+            }
+        })
+        binding.fMainUtilManagerBaseSwitcher.switcherIsChecked(true)
+
+        binding.fMainUtilManagerCustomSwitcher.setSwitchHandler(object : ISwitchHandler {
+            override fun onSwitchChange(isChecked: Boolean) {
+                val switcher = provideSwitchManagerForCustomSwitcher()
+                provideViewsForCustomSwitcher().forEach { view ->
+                    view?.let {
+                        if (isChecked)
+                            switcher.enableView(view)
+                        else
+                            switcher.disableView(view)
+                    }
+                }
+            }
+        })
+        binding.fMainUtilManagerCustomSwitcher.switcherIsChecked(true)
+    }
+
+    private fun provideSwitchManagerForCustomSwitcher(): SwitchManager {
+        return object : SwitchManager() {
+            override fun enableView(view: View): Boolean {
+                return when (view){
+                    is Button, is SwitchMaterial -> {
+                        view.isEnabled = true
+                        true
+                    }
+                    else ->
+                        super.enableView(view)
+                }
+            }
+
+            override fun disableView(view: View): Boolean {
+                return when (view){
+                    is Button, is SwitchMaterial -> {
+                        view.isEnabled = false
+                        false
+                    }
+                    else ->
+                        super.disableView(view)
+                }
+            }
+        }
+    }
+
+    private fun provideViewsForCustomSwitcher(): List<View?> {
+        return listOf(
+            binding.fMainUtilManagerCustomButton,
+            binding.fMainUtilManagerCustomEdit,
+            binding.fMainUtilManagerCustomTestSwitcher,
+            binding.fMainUtilManagerCustomSwitchMaterial
+        )
+    }
 }
