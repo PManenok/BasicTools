842d385189f145665fb878d85ff0d9aa80a37f56
PalinaManenok <p.manenok@alcosi.eu>
2024-09-27 22:59:04 +0300

    ABL-24: Update version

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,33 +19,33 @@ buildscript {
         set("repoName", "basetools")
 
         //Lib versions
-        set("loggerLib", "4.0.0") // independent (6)
-        set("utilLib", "4.0.0") // independent (1)
-        set("utilUiLib", "4.0.0") // independent (2)
-        set("timeparserLib", "4.0.0") // independent
-        set("pinviewLib", "4.0.0") // independent
-        set("inputfieldviewLib", "4.0.0") // independent
-        set("checkerLib", "4.0.0") // independent (3)
-        set("numpadLib", "4.0.0")
-        set("listheaderLib", "4.0.0")
-        set("dimenUtilLib", "4.0.0") // independent (1)
-        set("basedaggeruiLib", "4.0.0") // independent
-        set("cardlineLib", "4.0.0")
-        set("topbarviewLib", "4.0.0")
-        set("customswitchLib", "4.0.0")
+        set("loggerLib", "4.0.1") // independent (6)
+        set("utilLib", "4.0.1") // independent (1)
+        set("utilUiLib", "4.0.1") // independent (2)
+        set("timeparserLib", "4.0.1") // independent
+        set("pinviewLib", "4.0.1") // independent
+        set("inputfieldviewLib", "4.0.1") // independent
+        set("checkerLib", "4.0.1") // independent (3)
+        set("numpadLib", "4.0.1")
+        set("listheaderLib", "4.0.1")
+        set("dimenUtilLib", "4.0.1") // independent (1)
+        set("basedaggeruiLib", "4.0.1") // independent
+        set("cardlineLib", "4.0.1")
+        set("topbarviewLib", "4.0.1")
+        set("customswitchLib", "4.0.1")
 
-        set("recyclerLib", "4.0.0") // dimen_util
-        set("domainLib", "4.0.0") // logger (2)
-        set("errorMapperLib", "4.0.0")
-        set("dialogCoreLib", "4.0.0") // logger, util, checker (2)
-        set("dialogLib", "4.0.0") // dialogCoreLib(logger, util, checker (2))
+        set("recyclerLib", "4.0.1") // dimen_util
+        set("domainLib", "4.0.1") // logger (2)
+        set("errorMapperLib", "4.0.1")
+        set("dialogCoreLib", "4.0.1") // logger, util, checker (2)
+        set("dialogLib", "4.0.1") // dialogCoreLib(logger, util, checker (2))
         set(
             "dialogMessageLib",
-            "4.0.0"
+            "4.0.1"
         ) // dialogLib(dialogCoreLib(logger, util, checker (2))), recycler(dimen_util)
-        set("biometricLib", "4.0.0") //logger
-        set("baseuiLib", "4.0.0")
+        set("biometricLib", "4.0.1") //logger
+        set("baseuiLib", "4.0.1")
         //dialog(logger, util, checker, recycler(dimen_util)), domain(logger)
-        set("accesscontainerLib", "4.0.0") //logger
+        set("accesscontainerLib", "4.0.1") //logger
     }
 }
